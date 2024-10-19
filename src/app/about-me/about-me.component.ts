83107af8874f868ebfa5e81c20d313689ae5f2d7
mohib181 <mohib18169@gmail.com>
2024-10-20 02:02:26 +0600

    basic about me info added

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin, faGoogleScholar } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
-
+  profile_photo_src = 'assets/mohib_hossain_profile_picture.jpg'
+  
+  socialLinks = [
+    { link: 'https://github.com/mohib181', icon: faGithub},
+    { link: 'https://www.linkedin.com/in/mohib181', icon: faLinkedin},
+    { link: 'https://scholar.google.com/citations?user=jChTL-AAAAAJ', icon: faGoogleScholar}
+  ]
 }
