8f8f2e80b2dc45ec88e88b8300f9ce1d3f2ada5a
mohib181 <mohib18169@gmail.com>
2024-10-19 22:21:55 +0600

    navbar implemented

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  menuOpen: boolean = false;
 
+  navItems = [
+    { name: 'About Me', route: '/about' },
+    { name: 'Work', route: '/work' },
+    { name: 'Research', route: '/research' },
+    { name: 'Education', route: '/education' },
+    { name: 'Skills', route: '/skills' },
+    { name: 'Projects', route: '/projects' },
+    { name: 'CV', route: '/cv' }
+  ];
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen; // Toggle the menu on mobile
+  }
 }
