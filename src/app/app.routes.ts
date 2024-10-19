9b9341fb7066b0f7a27fba8f9b4510591cbf2b2a
mohib181 <mohib18169@gmail.com>
2024-10-19 22:46:26 +0600

    initialized about me component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'about',
+        component: AboutMeComponent,
+        title: 'About Me'
+    }
+];
