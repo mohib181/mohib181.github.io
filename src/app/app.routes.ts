f3dd93a030aad8d18c1107e323f0e8088fe563d6
mohib181 <mohib18169@gmail.com>
2024-10-20 22:10:14 +0600

    initialised work component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,11 @@ export const routes: Routes = [
         path: 'about',
         component: AboutMeComponent,
         title: 'About Me'
+    },
+    {
+        path: 'work',
+        component: WorkExperienceComponent,
+        title: 'Work Experience'
     },
 
     // Wildcard route to catch unknown paths and redirect to homepage
